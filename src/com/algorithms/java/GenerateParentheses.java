3715ebb43c1bdb69e3b7113c32086d871543f593
Amit Mangal <>
2022-07-10 12:32:42 +0530

    Four Sum

---
+++ src/com/algorithms/java/GenerateParentheses.java
@@ -0,0 +1,13 @@
+package com.algorithms.java;
+
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main (String[] args) {
+
+    }
+
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
