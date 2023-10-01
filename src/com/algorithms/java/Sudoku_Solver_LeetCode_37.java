304516a1c74f915e714c12c568633f1f448c4618
Amit Mangal <>
2023-10-01 22:34:08 +0530

    changes

---
+++ src/com/algorithms/java/Sudoku_Solver_LeetCode_37.java
@@ -0,0 +1,4 @@
+package com.algorithms.java;
+
+public class Sudoku_Solver_LeetCode_37 {
+}
--- src/com/algorithms/java/The_kth_Factor_Of_N_LeetCode_1492.java
+++ src/com/algorithms/java/The_kth_Factor_Of_N_LeetCode_1492.java
@@ -1,24 +1,75 @@
 package com.algorithms.java;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class The_kth_Factor_Of_N_LeetCode_1492 {
 
     public static void main (String[] args) {
         int  n = 4, k = 4;
-        System.out.println("kth Factor = " + kthFactor(n, k));
+        System.out.println("kth Factor = " + kthFactorOptimizedSqrt(n, k));
     }
 
     public static int kthFactor(int n, int k) {
           int sqrtRoot = (int) Math.sqrt(n);
+          ArrayList<Integer> list = new ArrayList<>();
           for (int i=1; i<=sqrtRoot; i++) {
-              if (n%i ==0 && --k ==0) {
-                  return n/i;
+              int divisor = n%i ;
+              if (divisor == 0) {
+                  int quotient = n/i ;
+                  --k;
+                  if (k==0) {
+                      return i;
+                  } else {
+                      if (quotient != i) {
+                          list.add(quotient);
+                      }
+                  }
               }
           }
-
-          for (int i=sqrtRoot+1; i>1; i--) {
-              
+          if (list.size()==0 || k > list.size()) {
+              return -1;
+          } else {
+              return list.get(list.size() - k);
           }
 
-          return -1;
+    }
+   /*
+        Efficient Approach: The problem can be solved in an optimized way in sqrt(n) complexity by using the
+        fact that factors of any number remain in the form of pairs.
+        In other words, if i is a factor of number n then n/i  will also be a factor of n.
+   */
+    public static int kthFactorOptimizedSqrt(int n, int k) {
+        int d = 1;
+        for (; d * d <= n; ++ d) {
+            if (n % d == 0) {
+                if (-- k == 0) {
+                    return d;
+                }
+            }
+        }
+        for (d = d - 1; d >= 1; -- d) {
+            if (d * d == n) continue;
+            if (n % d == 0) {
+                if (-- k == 0) {
+                    return n / d;
+                }
+            }
+        }
+        return -1;
+    }
+   //Brute Force approach
+    public static int kthFactorSeconIteration(int n, int k) {
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+            if(count==k){
+                return i;
+            }
+        }
+        return -1;
     }
 }
